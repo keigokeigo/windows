4f95380ec6594792585462355954ac69f23906d5
keigokeigo <keigo.ito@gmail.com>
2016-04-19 09:36:21 +0900

    Update del-old-files.bat
    Add /q flag.

--- del-old-files.bat
+++ del-old-files.bat
@@ -1,5 +1,5 @@
 @echo off
 set DIR=%HOMEDRIVE%%HOMEPATH%\Downloads
 
-forfiles /S /P %DIR% /D -60 /M "*.*" /c "cmd /c del /f @path"
+forfiles /S /P %DIR% /D -60 /M "*.*" /c "cmd /c del /q /f @path"
 forfiles /S /P %DIR% /D -60 /c "cmd /c if @isdir==TRUE rmdir/s/q @path"
